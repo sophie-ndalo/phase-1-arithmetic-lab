fc7513d1625613f11631fb7e259085f2818a1b42
sophia <sophiendalo@gmail.com>
2022-11-23 15:26:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1=31;
+const num2=2;
+const multiply =(31*2);
 
-// Write your code here
+const random = Math.ceil(Math.random());
+
+const num3=16;
+const num4=6;
+const mod = (16%6);
+
+const max= (20);
